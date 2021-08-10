5445b1025cb42e436bc749455ad63660e001de2e
jacobkim9881 <jacobkim9881@gmail.com>
2021-08-10 18:32:33 +0900

    feat: added scale at other site: 82cook, ruriweb

--- give-event.js
+++ give-event.js
@@ -1,42 +1,57 @@
 function giveEvent() {
-  let aTag1 = document.querySelectorAll('a');
+  let aTag1 = document.querySelectorAll('a[href]');
   let isPreTitle
     , isNeTitle
     , pHref
     , nHref
     , titleClass
-    , targetTitle
-    , previousTitle
-    , nextTitle;
- 
+    , targetTitle;
+  console.log(localStorage.getItem('board-swiper-class-name'));
+    titleClass = document.getElementsByClassName(localStorage.getItem('board-swiper-class-name'));	  
   aTag1.forEach((aTag) => {
-    //	 console.log(aTag)
-    titleClass = document.querySelectorAll('.' + localStorage.getItem('board-swiper-class-name'));
+//console.log(aTag)
+//console.log(titleClass);
     targetTitle = localStorage.getItem('board-swiper-current-title');
-    //console.log(aTag.innerText, targetTitle);	   
+    //console.log(aTag.innerText, targetTitle);	 
+    if (titleClass.length > 0 ) {	 
     if (aTag.innerText.trim() === targetTitle || aTag.innerText === targetTitle ) {  
 		 console.log(aTag.innerText[1], targetTitle[1]);
 	 console.log(aTag.innerText[1] === targetTitle[1]); 
 	      console.log(targetTitle);
-      let classIndex = Array.from(titleClass).indexOf(aTag);
+      let tdTag
+        , classIndex
+	, aTagIndex
+	, pAtagIndex
+	, nAtagIndex    
+	, previousAtag
+	, nextAtag;
+      tdTag = aTag.closest('td');	    
+      classIndex = Array.from(titleClass).indexOf(tdTag);
+      aTagIndex = Array.from(tdTag.querySelectorAll('a')).indexOf(aTag);	    
+      pAtagIndex = Array.from(titleClass[classIndex + 1].querySelectorAll('a')).indexOf(aTag);	    
+      nAtagIndex = Array.from(titleClass[classIndex - 1].querySelectorAll('a')).indexOf(aTag);	    
+      previousAtag = titleClass[classIndex + 1].querySelectorAll('a')[aTagIndex];
+      nextAtag = titleClass[classIndex - 1].querySelectorAll('a')[aTagIndex];
       console.log(classIndex);	
-      console.log(titleClass[classIndex - 1]);	      
-      localStorage.setItem('board-swiper-previous', titleClass[classIndex + 1].href);
-      localStorage.setItem('board-swiper-next', titleClass[classIndex - 1].href);
-      localStorage.setItem('board-swiper-previous-title', titleClass[classIndex + 1].innerText);
-      localStorage.setItem('board-swiper-next-title', titleClass[classIndex - 1].innerText);
-      previousTitle = titleClass[classIndex + 1].innerText;
-      nextTitle = titleClass[classIndex - 1].innerText;
+      console.log(titleClass[classIndex - 1]);	    
+      console.log(previousAtag)	    
+      console.log(nextAtag)	   
+      localStorage.setItem('board-swiper-previous', previousAtag.href);
+      localStorage.setItem('board-swiper-next', nextAtag.href);
+      localStorage.setItem('board-swiper-previous-title', previousAtag.innerText);
+      localStorage.setItem('board-swiper-next-title', nextAtag.innerText);
+      
     }
-    aTag.addEventListener('click', (e) => {
+    }
+      aTag.addEventListener('click', (e) => {
       console.log(e)      	   
-      console.log(e.target.tagName);	    
-      let getClassName = e.target.className;
+      console.log(e.target.tagName);	   	    
+      let getClassName = e.target.closest('td').className;
 		    console.log(getClassName.length);
       console.log(getClassName);
       if (getClassName.length === 0) {return;};	    
-      titleClass = document.querySelectorAll('.' + getClassName);
-      let eIndex = Array.from(titleClass).indexOf(e.target);
+      titleClass = document.getElementsByClassName(getClassName);
+      let eIndex = Array.from(titleClass).indexOf(e.target.closest('td'));
       console.log(eIndex)	    
       console.log(titleClass[eIndex + 1]);    
       console.log(titleClass[eIndex - 1]);   
@@ -54,7 +69,8 @@ function giveEvent() {
         localStorage.setItem('board-swiper-class-name',  getClassName);
         localStorage.setItem('board-swiper-current-title', e.target.innerText);
       }
-
+      //e.preventDefault();
+      //return false;
     });
   });
 
