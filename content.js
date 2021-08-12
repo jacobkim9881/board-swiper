fca56a3403d22ccf54395f9d63f21fa01a7349da
jacobkim9881 <jacobkim9881@gmail.com>
2021-08-12 22:02:57 +0900

    test: added switch by hostname

--- content.js
+++ content.js
@@ -1,10 +1,21 @@
 console.log(localStorage.getItem('board-swiper-previous'));
 console.log(document.readyState);
-
+console.log(siteList[0]["custom-function"]);
+let hostUrl = window.location.hostname;
 window.addEventListener("load", function() {
   console.log(document);	
   console.log(document.readyState);
-  giveEvent();	
+
+  switch (hostUrl) {
+  case 'cafe.naver.com':
+		  console.log('hollo');
+  giveEvent2(siteList[0]["a-class"], siteList[0]["wraper-tag"], siteList[0]["wraper-class"]);
+  listEvent(siteList[0]["list-click-event"]["a-class"]);
+  return;
+  default :
+  giveEvent();		 
+  return;		  
+  }
 }, false);
 
 pageMover(document);
