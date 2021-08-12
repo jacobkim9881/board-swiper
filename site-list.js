6f3240afb8ad859333d084686ff4d60d75ca32f3
jacobkim9881 <jacobkim9881@gmail.com>
2021-08-12 10:55:59 +0900

    test: added obj for custom site

---
+++ site-list.js
@@ -0,0 +1,18 @@
+let siteList = 
+	
+  [
+    {
+     "host" : 'cafe.naver.com',
+	    "custom-function" : 1,
+	"wrapper-class": "tit-area",
+		    "wrapper-tag": "div",
+		    "a-class": "tit"
+	    "isListDifferent" : true,
+	    "list-click-event": [
+		    "a-class" : "article",
+	    "wrapper-class": "inner_list",
+	    "wrapper-tag": "div",
+	    ]
+    }
+  ]
+	
