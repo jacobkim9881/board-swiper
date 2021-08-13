68d97d87c476591c75e3d43713457c0b81503b1a
jacobkim9881 <jacobkim9881@gmail.com>
2021-08-13 11:35:00 +0900

    feat: bobaedream: wraper for author sibling

--- site-list.js
+++ site-list.js
@@ -4,15 +4,41 @@ let siteList =
     {
      "host" : 'cafe.naver.com',
 	    "custom-function" : 1,
-	"wrapper-class": "tit-area",
-		    "wrapper-tag": "div",
-		    "a-class": "tit"
+	"wraper-class": "tit_area",
+		    "wraper-tag": "div",
+		    "a-class": "tit",
 	    "isListDifferent" : true,
-	    "list-click-event": [
+	    "list-click-event": {
 		    "a-class" : "article",
-	    "wrapper-class": "inner_list",
-	    "wrapper-tag": "div",
-	    ]
+	    "wraper-class": "inner_list",
+	    "wraper-tag": "div",
+	    }
+    },
+  {
+     "host" : 'www.bobaedream.co.kr',
+	    "custom-function" : 3,
+	"wraper-class": "author",
+		    "wraper-tag": "td",
+		    "a-class": "",
+	    "isListDifferent" : true,
+	    "list-click-event": {
+		    "a-class" : "bsubject",
+	    "wraper-class": "pl14",
+	    "wraper-tag": "td",
+	    }
+    }
+ ,{
+     "host" : 'www.clien.net',
+	    "custom-function" : 4,
+	"wraper-class": "list_title",
+		    "wraper-tag": "div",
+		    "a-class": "list_subject",
+	    "isListDifferent" : false,
+	    "list-click-event": {
+	"wraper-class": "list_title",
+		    "wraper-tag": "div",
+		    "a-class": "list_subject",
+	    }
     }
   ]
 	
